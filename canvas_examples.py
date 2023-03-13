8aa1a7181f9b0187da0aded7d71d5827a07df8df
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-03-13 14:33:04 +0100

    exercice ball

--- canvas_examples.py
+++ canvas_examples.py
@@ -1,8 +1,9 @@
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.graphics.vertex_instructions import Line, Rectangle
+from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
 from kivy.graphics.context_instructions import Color
 from kivy.metrics import dp
+from kivy.properties import Clock
 
 
 Builder.load_file("canvas_examples.kv")
@@ -44,3 +45,44 @@ class CanvasExample4(Widget):
         
         x += inc
         self.rect.pos = (x,y)
+        
+
+class CanvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        self.vx = dp(3)
+        self.vy = dp(4)
+        with self.canvas:
+            self.ball = Ellipse(pos=(100,100), size=(self.ball_size,self.ball_size))
+        Clock.schedule_interval(self.update, 1/60) #refresh 60 par seconde
+    
+    def on_size_window(self, *args):
+        print("On size: " + str(self.width) + ", " + str(self.height))
+        self.ball.pos = (self.center_x-self.ball_size/2, self.center_y-self.ball_size/2)
+    
+    def update(self, dt): #les fonctions appellés par schedule ont tjs dt comme parametre
+        x, y = self.ball.pos
+        
+        x += self.vx
+        y += self.vy
+        
+        if (y+self.ball_size > self.height):
+            y = self.height-self.ball_size
+            
+            self.vy = -self.vy
+            
+        if (y < 0):
+            y = 0
+            self.vy = -self.vy
+            
+        if (x+self.ball_size > self.width):
+            x = self.width-self.ball_size
+            self.vx = -self.vx
+            
+        if (x < 0):
+            x = 0
+            
+            self.vx = -self.vx
+        
+        self.ball.pos = (x, y)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from navigation_screen_manager import NavigationScreenManager
 from kivy.properties import ObjectProperty
 # from canvas_examples import CanvasExample1, CanvasExample2, CanvasExample3
 from canvas_examples import * #permet de tout importer
+
 class MyScreenManager(NavigationScreenManager):
     pass
 
@@ -12,6 +13,6 @@ class LeLabApp(App):
     def build(self):
         self.manager = MyScreenManager()
         # return self.manager
-        return CanvasExample4()
+        return CanvasExample5()
 
 LeLabApp().run()
